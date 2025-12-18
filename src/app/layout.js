c2c113cab603c7d5de699b630dbf00d7ca62bb16
kodemapper <saranganilgade@gmail.com>
2025-12-18 22:49:21 +0530

    responsive to all screens done

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,7 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="dark" suppressHydrationWarning>
-      <body className={`${outfit.variable} ${playfair.variable}`}>
+      <body className={`${outfit.variable} ${playfair.variable}`} suppressHydrationWarning>
         {children}
       </body>
     </html>
